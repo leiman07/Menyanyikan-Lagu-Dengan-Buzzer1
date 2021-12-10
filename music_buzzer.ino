d27dfd340b458e403408e38e5cf4edeefc4da24a
Leiman GoVlog <31844101+leiman07@users.noreply.github.com>
2021-12-10 21:16:22 +0700

    Add files via upload

---
+++ music_buzzer.ino
@@ -0,0 +1,48 @@
+//melody super mario
+#include "pitches.h"
+
+// note dalam data melody:
+int melody[] = {
+
+  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+
+  4, 8, 8, 4, 4, 4, 4, 4
+};
+
+void setup() {
+  // kosong
+}
+
+void loop() {
+for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+    // to calculate the note duration, take one second divided by the note type.
+
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+
+    int noteDuration = 1000 / noteDurations[thisNote];
+
+    tone(D1, melody[thisNote], noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+
+    // the note's duration + 30% seems to work well:
+
+    int pauseBetweenNotes = noteDuration * 1.30;
+
+    delay(pauseBetweenNotes);
+
+    // stop musicnya;
+
+    noTone(D1);
+
+  }
+  // no need to repeat the melody.
+}
+//PROJECT : Program Menyanyikan Lagu Dengan Buzzer
+//DIBUAT  : TRIDIPI.PROJECT
+//TANGGAL : 07/12/2021
